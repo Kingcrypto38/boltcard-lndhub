f828930900f3d97f80fa82aa6ce4cc3a6fcdb509
Chloe Jung <chloe@onesandzeros.nz>
2023-03-03 16:42:18 +1300

    Update ground control code

--- controllers/api.js
+++ controllers/api.js
@@ -316,36 +316,34 @@ router.post('/payinvoice', postLimiter, async function (req, res) {
           lock.releaseLock();
 
           logger.log('/payinvoice sendPayment callback', payment);
-          if(payment.state == 'SETTLED') {
-            //send notification through ground control
-            const LightningInvoiceSettledNotification = {
-              payment_request: req.body.invoice,
-              amt_paid_sat: info.num_satoshis, // amt is used only for 'tip' invoices
-              userid: u.getLogin(),
-              memo: info.description
-            };
-            logger.log('/payinvoice', 'payment made by ', u.getLogin(), ', posting to GroundControl...');
-            console.log('payment made by ', u.getLogin(), ', posting to GroundControl...');
-            console.log(LightningInvoiceSettledNotification, ', posting to GroundControl...');
+          //send notification through ground control
+          const LightningInvoiceSettledNotification = {
+            payment_request: req.body.invoice,
+            amt_paid_sat: info.num_satoshis, // amt is used only for 'tip' invoices
+            userid: u.getLogin(),
+            memo: info.description
+          };
+          logger.log('/payinvoice', 'payment made by ', u.getLogin(), ', posting to GroundControl...');
+          console.log('payment made by ', u.getLogin(), ', posting to GroundControl...');
+          console.log(LightningInvoiceSettledNotification, ', posting to GroundControl...');
 
-            const baseURI = process.env.GROUNDCONTROL;
-            if (baseURI) {
-              const _api = new Frisbee({ baseURI: baseURI });
-              const apiResponse = await _api.post(
-                '/userPaidLightningInvoice',
-                Object.assign(
-                  {},
-                  {
-                    headers: {
-                      'Access-Control-Allow-Origin': '*',
-                      'Content-Type': 'application/json',
-                    },
-                    body: LightningInvoiceSettledNotification,
+          const baseURI = process.env.GROUNDCONTROL;
+          if (baseURI) {
+            const _api = new Frisbee({ baseURI: baseURI });
+            const apiResponse = await _api.post(
+              '/userPaidLightningInvoice',
+              Object.assign(
+                {},
+                {
+                  headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json',
                   },
-                ),
-              );
-              console.log('GroundControl:', apiResponse.originalResponse.status);
-            }
+                  body: LightningInvoiceSettledNotification,
+                },
+              ),
+            );
+            console.log('GroundControl:', apiResponse.originalResponse.status);
           }
 
           res.send(payment);
