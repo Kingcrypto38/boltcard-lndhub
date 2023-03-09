077d079eeb9c70b10eade2b59572b3697cd432b6
Chloe Jung <chloe@onesandzeros.nz>
2023-03-09 13:24:15 +1300

    On create wallet, send hub type as a response so the wallet app knows this is from the bolt card hub

--- controllers/api.js
+++ controllers/api.js
@@ -153,7 +153,7 @@ router.post('/create', postLimiter, async function (req, res) {
   let u = new User(redis, bitcoinclient, lightning);
   await u.create();
   await u.saveMetadata({ partnerid: req.body.partnerid, accounttype: req.body.accounttype, created_at: new Date().toISOString() });
-  res.send({ login: u.getLogin(), password: u.getPassword() });
+  res.send({ hub_type: 'boltcardhub', login: u.getLogin(), password: u.getPassword() });
 });
 
 router.post('/auth', postLimiter, async function (req, res) {
